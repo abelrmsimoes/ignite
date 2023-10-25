a5e1286db3620c3dbccb8c47658d4ef71d83ffde
Abel Simões <abelrmsimoes@gmail.com>
2023-10-25 02:07:26 -0300

    03. Streams no Node.js

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,27 +1,33 @@
-import http from 'node:http';
+import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-const users = [];
+const users = []
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  console.log(req.body)
 
   if (method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-Type', 'application/json')
-      .end(JSON.stringify(users));
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-    });
+      name,
+      email,
+    })
 
-    return res.writeHead(201).end();
+    return res.writeHead(201).end()
   }
 
-  return res.writeHead(404).end();
-});
+  return res.writeHead(404).end()
+})
 
-server.listen(3333);
+server.listen(3333)
